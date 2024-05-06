0abca057d87edfcc3d5c3814cae5646b64c9b746
Chien Chuan Wang <chien@chienchuan.com>
2024-05-06 14:19:28 +0800

    define app name, version, and warning texts

---
+++ todo_project/core/__init__.py
@@ -0,0 +1,24 @@
+# Define app name and app current version.
+__app_name__ = "todo"
+__version__ = "0.1.0"
+
+# Assign integer numbers to each variable of all the situation.
+# For instance, if we print SUCCESS, we will get 0 as result. If we print ID_ERROR, we will get 7.
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+) = range(7)
+
+# Use a dictionary to map code to human-readable messages.
+ERROR = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+    DB_READ_ERROR: "database read error",
+    DB_WRITE_ERROR: "database write error",
+    ID_ERROR: "to-do id error",
+}
