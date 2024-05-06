017ed12bbaac3b722a5684c62dae701d77049eaf
Chien Chuan Wang <chien@chienchuan.com>
2024-05-06 15:32:02 +0800

    add notes for each step

--- todo_project/core/cli.py
+++ todo_project/core/cli.py
@@ -2,23 +2,33 @@ from typing import Optional
 import typer
 from core import __app_name__, __version__
 
-app = typer.typer()
+# Creates an explicit Typer application, app.
+app = typer.Typer()
 
 
+# Define _version_callback().
+# This function takes a Boolean argument called value.
+# If value is True, then the function prints the application’s name and version using echo().
+# After that, it raises a typer.Exit exception to exit the application cleanly.
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
 
 
+# Define main() as a Typer callback using the @app.callback() decorator.
+@app.callback()
 def main(
+    # define main() as a Typer callback using the @app.callback() decorator.
+    # This means it can be either of bool or None type.
+    # The version argument defaults to a typer.Option object, which allows you to create command-line options in Typer.
     version: Optional[bool] = typer.Option(
         None,
-        "--version",
+        "--version",  # Set the command-line names for the version option: -v and --version.
         "-v",
-        help="Show the application's version and exit.",
-        callback=_version_callback,
-        is_eager=True,
+        help="Show the application's version and exit.",  # Provides a help message for the version option.
+        callback=_version_callback,  # Attaches a callback function, _version_callback(), to the version option, which means that running the option automatically calls the function.
+        is_eager=True,  # Sets the is_eager argument to True. This argument tells Typer that the version command-line option has precedence over other commands in the current application.
     )
 ) -> None:
     return
