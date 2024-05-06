70446f7dcaaa619ff7c6f9cf02fa06220f5145c9
Chien Chuan Wang <chien@chienchuan.com>
2024-05-06 14:20:48 +0800

    using typer to define version callback and other option commands

---
+++ todo_project/core/cli.py
@@ -0,0 +1,24 @@
+from typing import Optional
+import typer
+from core import __app_name__, __version__
+
+app = typer.typer()
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
