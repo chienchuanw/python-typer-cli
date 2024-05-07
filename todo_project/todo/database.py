b0560a8dfa4b4bafa2c10620bc93b73054ce49f3
Chien Chuan Wang <chien@chienchuan.com>
2024-05-07 15:17:39 +0800

    add notes in database.py

--- todo_project/todo/database.py
+++ todo_project/todo/database.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 from todo import DB_WRITE_ERROR, SUCCESS
 
+# define DEFAULT_DB_FILE_PATH to hold the default database file path. The application will use this path if the user doesn’t provide a custom one.
 DEFAULT_DB_FILE_PATH = Path.home().joinpath("." + Path.home().stem + "_todo.json")
 
 
+# define get_database_path(). This function takes the path to the app’s config file as an argument, reads the input file using ConfigParser.read(), and returns a Path object representing the path to the to-do database on your file system. The ConfigParser instance stores the data in a dictionary. The "General" key represents the file section that stores the required information. The "database" key retrieves the database path.
 def get_database_path(config_file: Path) -> Path:
     """Return the current path to the to-do database."""
     config_parser = configparser.ConfigParser()
@@ -13,6 +15,7 @@ def get_database_path(config_file: Path) -> Path:
     return Path(config_parser["General"]["database"])
 
 
+# define init_database(). This function takes a database path and writes a string representing an empty list. You call .write_text() on the database path, and the list initializes the JSON database with an empty to-do list. If the process runs successfully, then init_database() returns SUCCESS. Otherwise, it returns the appropriate error code.
 def init_database(db_path: Path) -> int:
     """Create the to-do database."""
     try:
